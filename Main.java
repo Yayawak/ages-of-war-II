4e23530584b845bcb7385d5769a5729874f783cf
Yawa <61963667+Yayawak@users.noreply.github.com>
2023-02-23 00:50:01 +0700

    try add test gui for better life

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import TestUI.MainUI;
 import srcs.App;
 
 class Main {
     public static void main(String[] args) {
         // App a1 = new App();
-        new App();
+        // new App();
+        new MainUI().setVisible(true);
     }
 }
